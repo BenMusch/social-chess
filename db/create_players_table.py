09e169153cace552f76413c9e21758b02cb1fbf4
edfactor <ejlyons@ix.netcom.com>
2019-04-11 17:38:20 -0400

    Create and load data

--- db/create_players_table.py
+++ db/create_players_table.py
@@ -5,27 +5,27 @@ import sqlite3 as sqlite
 
 con = sqlite.connect('chess.db')
 
+players = (
+    (1,  'John Smith', 'Statehouse News', 3, 0),
+    (2,  'Michael Newsome', 'State Senator', 2, 1),
+    (3,  'Charlie Baker', 'Governor', 1, 1),
+    (4,  'Terry McFeeney', 'Boston Globe', 2, 0),
+    (5,  'Winnie McStooge', 'Activist', 1, 0),
+    (6,  'Toady Longfellow', 'Boston Magazine', 1, 0),
+    (7,  'Marcus More', 'SJC Judge', 3, 1),
+    (8,  'Vernon Smithereens', 'WGBH', 1, 0),
+    (9,  'Jodi Reinhardt', 'MassINC', 1, 0),
+    (10, 'Maxine Vilamont', 'Activist', 1, 0),
+    (11, 'Keefe Ochabi', 'Dorchester Reporter', 2, 0),
+    (12, 'Tsunami Tanaka', 'Pretty Good Polling', 2, 0),
+    (13, 'Radar O\'Relly', 'Boston Phoenix', 2, 0),
+    (14, 'Manfred Jackson', 'WBUR', 2, 0),
+)
+
 with con:
 
     cur = con.cursor()
-
+    cur.execute("DROP TABLE IF EXISTS players")
     cur.execute("CREATE TABLE players(id INT, name TEXT, affiliation TEXT, level INT, vip INT)")
 
-    # This will be dummy data
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
-
+    cur.executemany("INSERT INTO players VALUES(?, ?, ?, ?, ?)", players)
--- harness.py
+++ harness.py
@@ -1,3 +1,24 @@
 """
 This is the command-line tool that drives the creation of tournament pairings. 
 """
+import sqlite3 as sqlite
+from chess_entities import Player
+
+players = []
+
+
+def load_players():
+
+    con = sqlite.connect('chess.db')
+
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM players")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(f"{row[0]} {row[1]} {row[2]} {row[3]} {row[4]}")
+
+            players.append(Player(row[1],level=int(row[3]), late=False, vip=(1 == int(row[4]))))
+
