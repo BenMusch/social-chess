e38ce5db8f48159c5155177ba78b2540de376043
edfactor <ejlyons@ix.netcom.com>
2019-04-11 16:33:25 -0400

    Building test data

--- db/create_players_table.py
+++ db/create_players_table.py
@@ -9,8 +9,23 @@ with con:
 
     cur = con.cursor()
 
-    cur.execute("CREATE TABLE players(id INT, name TEXT, level INT, vip INT)")
+    cur.execute("CREATE TABLE players(id INT, name TEXT, affiliation TEXT, level INT, vip INT)")
 
     # This will be dummy data
-    cur.execute("INSERT INTO players VALUES(1,'John Smith',3,0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
+    cur.execute("INSERT INTO players VALUES(1,'John Smith', 'Statehouse News', 3, 0)")
 
