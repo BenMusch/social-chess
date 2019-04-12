f02b789db04c62ae8336dddef510f8582f789e62
edfactor <ejlyons@ix.netcom.com>
2019-04-12 19:06:38 -0400

    Revise entity structure for rounds

--- chess_entities.py
+++ chess_entities.py
@@ -1,8 +1,8 @@
-import math
-
 """
 This module contains all of the objects we need
 """
+
+import math
 
 
 class Player(object):
@@ -23,7 +23,7 @@ class Player(object):
         return "{}({})".format(self._name, self._level)
 
 
-    def __init__(self, name, level, late=False, vip=False):
+    def __init__(self, name, level=1, late=False, vip=False):
         self._name = name
         self._level = level
         self._late = late
@@ -46,7 +46,7 @@ class Player(object):
         return self._vip
 
 
-class Match(object):
+class Game(object):
     """
     This will represent a game
     """
@@ -58,7 +58,7 @@ class Match(object):
     BLACK_WINS = 1
     DRAW = 2
 
-    _result = DRAW
+    _result = None
 
     def __str__(self):
         return "White:{} Black:{} ".format(self._white_player, self._black_player)
@@ -68,13 +68,22 @@ class Match(object):
         self._black_player = black
         self._game_time = time
 
+        # We need to test for a bye
+        if self._white_player.name == "Bye":
+            self._result = self.BLACK_WINS
+        elif self._black_player.name == "Bye":
+            self._result = self.WHITE_WINS
+
+
+    def is_game_over(self):
+        return self._result is not None
+
     def get_white_player(self):
         return self._white_player
 
 
     def get_black_player(self):
         return self._black_player
-
 
 
     def set_time(self, time):
@@ -88,6 +97,40 @@ class Match(object):
 
     def get_result(self):
         return self._result
+
+
+class Round(object):
+    """
+    A round is actually two sets of games, where everyone plays
+    or gets a bye. Normally, a round would be a set of games,
+    but a key requirement for such a social event is that people
+    need a break between games to eat, drink, and socialize.
+    
+    This means that only half play at once. So a round is two 
+    sets of games exercising all the players. We will refer
+    to the two sets of games as A and B. 
+    
+    """
+    
+    # There will be a list of Games for the first set
+    # and one for the second. It is important to know
+    # that these lists may not be the same size, to
+    # accommodate unusual numbers of players
+
+    a_games = []
+    b_games = []
+
+    def __init__(self, number_a_games, number_b_games):
+        """
+        To set this up, we're going to initialize the arrays
+        with  the correct number of games
+
+        :param number_a_games:
+        :param number_b_games:
+        """
+        self.a_games = [None] * number_a_games
+        self.b_games = [None] * number_b_games
+
 
 
 class Schedule(object):
@@ -117,12 +160,6 @@ class Schedule(object):
     # This is the data structure that will hold the results
     # It will be an two-dimensional array. An array of rounds
 
-    """
-     rounds = 
-     [ Match, Match, Match, Match, Match, Match ]
-     ...
-     [ Match, Match, Match, Match, Match, Match ]
-    """
 
     # We're going to keep track of this at the class level for convenience
     _total_number_of_players = 0
@@ -131,12 +168,12 @@ class Schedule(object):
     _intermediate_players = []
     _beginner_players = []
 
-
+    # This is a list of the two-part rounds we will have
     _rounds = list()
 
     def __repr__(self):
         return_line = "Schedule Object:\n"
-        return_line += "Number of boards:{} Number of minutes in the tournament:{} Gap in minutes between rounds:{}" \
+        return_line += "Number of boards:{} Number of minutes in the tournament:{} Gap in minutes between each set of games:{}" \
                         " Time in minutes for each game:{} " \
                         "Team Play?:{} Have Playoff?:{}\n" \
                         "".format(self._number_boards, self._number_minutes, self._rounds_gap, self._game_time,
@@ -153,17 +190,29 @@ class Schedule(object):
         count = 0
 
         print("There are {} rounds in the array.\n".format(len(self._rounds)))
-        for individual_round in self._rounds:
+        for two_part_round in self._rounds:
             print("Getting list of matches in round {}\n".format(count))
             return_line += "Round: {}\n".format(count + 1)
             return_line += "******************\n"
             board_counter = 0
-            for match in individual_round:
+
+            # First set
+            for match in two_part_round[0]:
                 return_line += "BOARD {} -- White: {} Black: {} \n".format(board_counter + 1, match.get_white_player(),
                                                                            match.get_black_player())
                 board_counter += 1
             count += 1
             return_line += "-----\n"
+
+            # Second set
+            board_counter = 0
+            for match in two_part_round[1]:
+                return_line += "BOARD {} -- White: {} Black: {} \n".format(board_counter + 1, match.get_white_player(),
+                                                                           match.get_black_player())
+                board_counter += 1
+            count += 1
+            return_line += "-----\n"
+
         return_line += "-----\n"
         return return_line
 
@@ -197,7 +246,7 @@ class Schedule(object):
 
             for m in range(0, boards):
                 print ("Adding match #{}\n".format(m))
-                current_round_list.append(Match())
+                current_round_list.append(Game())
 
             print("We are done with the round.")
 
