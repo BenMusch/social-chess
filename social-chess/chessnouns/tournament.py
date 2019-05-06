52c69778b3460287f83af4f54fc76928dd7b304d
edfactor <ejlyons@ix.netcom.com>
2019-05-06 00:17:41 -0400

    Moved tiebreaker logic into own module

--- social-chess/chessnouns/tournament.py
+++ social-chess/chessnouns/tournament.py
@@ -6,7 +6,7 @@ from . import slot
 from . import player
 from . import game
 from datetime import date
-from chessutilities import utilities
+from chessutilities import tiebreakers
 import logging
 import logging.config
 
@@ -42,12 +42,13 @@ class Tournament(object):
         count = 1
         logger.debug("Creating random results in round {}".format(count))
         for ind_round in rounds:
-            for ind_game in ind_round:
-                # logger.debug("Setting result for game: {} ".format(ind_game))
-                ind_game.set_likely_random_result()
+            self.create_random_results_for_round(ind_round)
 
-    def create_random_results_for_round(self):
-        pass
+    def create_random_results_for_round(self, tournament_round):
+
+        for ind_game in tournament_round:
+            logger.debug("Setting result for game: {} ".format(ind_game))
+            ind_game.set_likely_random_result()
 
     def return_result_numbers(self):
         """
@@ -89,12 +90,8 @@ class Tournament(object):
 
         """
 
-        # FIXME: We need to check to see that results got created before
-        # doing this
-
         slot_list = []
         for player_key, draw in self._tournament_draw_dict.items():
-            # tourney_player = utilities.get_player_for_id(player_key)
             tourney_player = self._schedule.get_scheduled_player_for_id(player_key)
             raw_points = draw.get_total_raw_points()
             weighted_points = draw.get_total_weighted_score()
@@ -103,6 +100,7 @@ class Tournament(object):
                                        str(round(weighted_points, chessnouns.WEIGHTED_SCORE_DECIMAL_PRECISION))))
 
         if maximum_number == 0:
+            # We are saying 0 means return all of them
             return sorted(slot_list)
         else:
             # So we only want the top X players
@@ -113,208 +111,75 @@ class Tournament(object):
         Here we are trying to figure out the top two people,
         or, if there are ties, the people tied for the top
         two slots
-        :return:
+        :return bool, list - we are returning True if we used
+        tiebreaks, false if not, and then a list of finalists
         """
+
+        # We want to track how often we use tiebreaks, so we
+        # will return one of these values. We are using
+        # these variable names to make the code more readable
+        # and less subject to error
+
+        we_used_tiebreaks = True
+        did_not_use_tiebreaks = False
 
         finalists = []
 
         # First, let's get the list
         leader_list = sorted(self.get_leaderboard())
 
-        top_person = leader_list[0]
+        logger.info("\nHere is the leaderboard going into this:\n")
+        logger.info(leader_list)
 
+        """ We are going to get the first person, who is
+        going to have the highest score, but they might 
+        not be the only person with that score. """
+        top_person = leader_list[0]
         top_score = top_person.get_weighted_score()
         logger.debug("Top score was: {}".format(top_score))
 
-        finalists.append(top_person)
+        """What we need to know now is if he's alone
+         at the top of the leaderboard."""
 
         next_person = leader_list[1]
         next_score = next_person.get_weighted_score()
         logger.debug("Next score was: {}".format(next_score))
 
-        finalists.append(next_person)
+        third_person = leader_list[2]
+        third_score = third_person.get_weighted_score()
+        logger.debug("Third score was: {}".format(third_score))
 
-        # Now we have to figure out if the next person
+        # Let us see if the first person is alone
+        if top_score > next_score:
+            # OK, leader is alone, so add him
+            finalists.append(top_person)
 
-        remaining_list = leader_list[2:]
-
-        for possible_person in remaining_list:
-            if possible_person.get_weighted_score() == next_score:
-                finalists.append(possible_person)
+            # But now we need to know if there are ties beneath him
+            if next_score > third_score:
+                # Great, then we can just add the second and be done
+                finalists.append(next_person)
+                return did_not_use_tiebreaks, finalists
             else:
-                break
-
-        player_break = False
-
-        if len(finalists) > 2:
-            logger.debug("We have more than two finalists with scores: ")
-            for final_person in finalists:
-                logger.debug(str(final_person.get_weighted_score()))
-            return self._try_to_resolve_finalists(finalists)
-
-        return player_break, finalists
-
-    def _try_to_resolve_finalists(self, finalists_slots):
-
-        # FIXME: We need to be careful about how draws are scored
-
-        logger.debug("PLAYERS WE ARE RESOLVING:")
-        for final_slot in finalists_slots:
-            logger.debug(final_slot.get_player().get_name() + " | " + str(final_slot.get_player().get_level()))
-
-        change = False
-        new_finalists = []
-
-        # The logic here isn't easy.
-        # Let's first determine if the leader is alone
-        top_score = finalists_slots[0].get_weighted_score()
-        second_score = finalists_slots[1].get_weighted_score()
-
-        if top_score > second_score:
-            # OK, so the top guy is alone
-            new_finalists.append(finalists_slots[0])
-            logger.info("So our clear winner was: {}".format(finalists_slots[0].get_player().get_name()))
-            # We also need to remove him
-            finalists_slots.remove(finalists_slots[0])
-
-            # OK, let's see how many others
-            if len(finalists_slots) == 2:
-                # So we only have two left
-                # Let's see if they played
-                second_player = finalists_slots[0].get_player()
-                logger.debug("Second player was: {}".format(second_player.get_name()))
-                third_player = finalists_slots[1].get_player()
-                logger.debug("Third player was: {}".format(third_player.get_name()))
-                played_game = self._schedule.get_common_game(second_player, third_player)
-
-                if played_game:
-
-                    if played_game.was_drawn():
-                        # Ugh.
-                        # OK, let's see if they were the same level
-                        if second_player.get_level() < third_player.get_level():
-                            # OK, that means we give the second player a performance bonus
-                            # as the underdog with same points, and he gets the slot
-                            logger.info("We used a performance bonus: second greater than third in a draw")
-                            new_finalists.append(second_player)
-                            change = True
-                            return change, new_finalists
-                        elif third_player.get_level() < second_player.get_level():
-                            # So the other guy gets the performance bonus
-                            logger.info("We used a performance bonus: third greater than second in a draw")
-                            new_finalists.append(third_player)
-                            change = True
-                            return change, new_finalists
-                        else:
-                            # They are the same level also. So we fail
-                            logger.info("They were equal in the draw, no performance bonus")
-                            new_finalists += [second_player, third_player]
-                            return change, finalists_slots
-                    elif played_game.did_player_id_win(second_player.get_id()):
-                        logger.info("Second beat third. We broke a tie with the played function")
-                        new_finalists.append(finalists_slots[1])
-                        change = True
-                        return change, new_finalists
-                    else:
-                        logger.info("Third beat second. We broke a tie with the played function")
-                        new_finalists.append(finalists_slots[2])
-                        change = True
-                        return change, new_finalists
-
-                else:
-                    # So they didn't play
-                    # Let's see if we can do a performance bonus
-                    if second_player.get_level() < third_player.get_level():
-                        # OK, that means we give the second player a performance bonus
-                        # as the underdog with same points, and he gets the slot
-                        logger.info("We used a performance bonus - second was lower than third")
-                        new_finalists.append(second_player)
-                        change = True
-                        return change, new_finalists
-                    elif third_player.get_level() < second_player.get_level():
-                        # So the other guy gets the performance bonus
-                        logger.info("We used a performance bonus = third was lower than second")
-                        new_finalists.append(third_player)
-                        change = True
-                        return change, new_finalists
-                    else:
-                        # They are the same level also. So we fail
-                        logger.info("Didn't play a game. They were equal, no performance bonus")
-                        new_finalists += [second_player, third_player]
-                        return change, new_finalists
-
-            if len(finalists_slots) > 2:
-                # So we have a top person, and more than 2 runners-up
-                # Egad.
-                # OK, let's see if we can grab one by being an upset
-                # candidate
-
-                # Let's see if we can sort the three other players
-                # by who they lost against
-
-                # First, let's see if there are differences:
-                logger.info("We have more than 2 after the top person")
-
-                for item in finalists_slots:
-                    logger.info(str(item))
-                losses_total = 0
-                for finalist_slot in finalists_slots:
-                    candidate_player = finalist_slot.get_player()
-                    logger.info("Player is {}".format(candidate_player))
-                    candidate_draw = candidate_player.get_draw()
-                    logger.info("Draw was {}".format(candidate_draw))
-                    losses_total += candidate_draw.get_total_loss_points()
-
-                if losses_total % len(finalists_slots) == 0:
-                    # OK, so no differences in losses, we fail
-                    logger.info("There were no differences after accounting for losses")
-                    return change, finalists_slots
-                else:
-                    # OK, so we have some differences
-                    logger.info("Let's get the list sorted by the losses.")
-                    new_list = sorted(finalists_slots, key=self._get_points_for_player, reverse=False)
-                    for lost_slot in new_list:
-                        lost_player = lost_slot.get_player()
-                        logger.info("Player: {}, Loss points: {}".format(lost_player.get_name(),
-                                                                         lost_player.get_draw().get_total_loss_points()))
-                        logger.info(lost_player.get_draw().output_win_loss_record())
+                # So we are in a situation where the first guy is alone,
+                # but the next two (or more) are tied
+                # So let's pop off the first guy, and send the list
+                # for processing
+                leader_list.pop(0)
+                return we_used_tiebreaks, tiebreakers.get_one_playoff_contender_from_all_tied(leader_list)
 
 
-                return change, finalists_slots
+        # So now we are looking at the possibility of multiple people tied at the top
+        elif top_score == next_score:
 
-        else:
-            # Ugh, they are tied. Worse, that means
-            # all of them are tied. This means we
-            # need to see if any played each other
+            # We need to see if the third one is lower,
+            #    simplifying things
+            if next_score > third_score:
+                finalists.append(top_person)
+                finalists.append(next_person)
+                return did_not_use_tiebreaks, finalists
+            else:
+                # Everyone is tied at the top
+                # We will use a function to resolve
+                return we_used_tiebreaks, tiebreakers.get_two_playoff_contenders_from_all_tied(finalists)
 
-            # For now, let's try upset candidates
-            return change, finalists_slots
 
-        return change, new_finalists
-
-    def _get_points_for_player(self, item):
-        return item.get_player().get_draw().get_total_loss_points()
-
-    def _look_for_upset_candidate(self, candidate_list):
-
-        found = False
-
-        number_found = 0
-
-        # First, we need to find out if there are any difference
-        # among the levels, for any number of candidates
-        total = 0
-
-        for candidate in candidate_list:
-            total += candidate.get_level()
-
-        # OK, now we should be able to see
-        if total % len(candidate_list) == 0:
-            # We're dead. They're all the same
-            return False, None
-
-        else:
-            # Good, we have at least some difference
-            pass
-
-        return found, player
